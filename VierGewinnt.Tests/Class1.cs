2d768dcb8406d2202e376da7925776e30c7fbcb7
Yann <y.hortance@interfama.de>
2015-12-08 12:18:52 +0100

    first unit Test

---
+++ VierGewinnt.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace VierGewinnt.Tests
+{
+
+    public class Class1
+    {
+        [Fact]
+        public void Should_Test_Something_Useful()
+        {
+            Assert.True(true);
+        }
+    }
+}
